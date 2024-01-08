50d78368fd349208576ebe11909c6727c4ba15e8
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2024-01-08 13:31:21 +0530

    Adding Files

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Row from './Components/Row.js'
 import requests from './requests.js';
+import Banner from './Components/Banner.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <h1>Hey hello</h1>
+
+      <Banner />
+
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       
     </div>
   );
